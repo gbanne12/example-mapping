0ee1eb71f8537ab521a1437161ec57801977f6fc
gbannermanIncremental <gary.bannerman@incrementalgroup.co.uk>
2023-03-29 15:56:05 +0100

    Split drop func into some helper methods

--- src/scripts/actions/Drag.ts
+++ src/scripts/actions/Drag.ts
@@ -1,43 +1,60 @@
+/** Class for handling draggable elements and available drop zones */
 export class Drag {
 
+    /**  Handles start of a drag operation. */
     public start(event: DragEvent) {
         if (event.target instanceof Element) {
             event.dataTransfer.setData('text/plain', event.target.id);
         }
     }
 
+    /** Handles when the drag event is over the drop area */
     public dragOver(event: DragEvent) {
         event.preventDefault();
         event.stopPropagation();
     }
 
-    public drop(event: DragEvent) {
-        const draggableElement = document.getElementById(event.dataTransfer.getData('text'));
+    /** Handles when an item is dropped on the drop zone.
+     *  Attaches the draggable to the nearest span.rules element.
+     *  Increases the containing div height if required. */
+    public drop = (event: DragEvent) => {
+        this.appendToRule(event);
+        this.adjustHeight(event);
+        event.dataTransfer.clearData();
+    }
+
+    private appendToRule(event: DragEvent): void {
         if (event.target instanceof Element) {
-            let dropzone: Element = event.target;
-            const isRuleSpan = dropzone.tagName == "span" && dropzone.getAttribute('class') == 'rules';
-            if (!isRuleSpan) {
-                dropzone = event.target.closest('span.rules');
+            let target = event.target;
+            const isRuleNote = target.tagName == "span" && target.getAttribute('class') == 'rules';
+            if (!isRuleNote) {
+                target = event.target.closest('span.rules');
             }
 
-            dropzone.appendChild(draggableElement);
+            const draggableId = event.dataTransfer.getData('text');
+            const draggableElement = document.getElementById(draggableId);
+            target.appendChild(draggableElement);
+        }
+    }
 
-            let childSpans = 0;
-            const children = dropzone.children;
+    private adjustHeight(event: DragEvent): void {
+        if (event.target instanceof Element) {
+            let target = event.target;
+
+            let total = 0;
+            const children = target.children;
             for (let child of children) {
                 if (child.nodeName.toLowerCase() == 'span') {
-                    childSpans++;
+                    total++;
                 }
             }
 
             const initialDivHeight = 300;
-            const requiredHeight = initialDivHeight + (childSpans * 175);
-            const containerDiv = dropzone.parentElement.parentElement.parentElement.parentElement;
-            if (containerDiv.clientHeight < requiredHeight) {
-                containerDiv.setAttribute('style', 'height:' + requiredHeight + 'px');
+            const newHeight = initialDivHeight + (total * 175);
+            const containerDiv = target.parentElement.parentElement.parentElement.parentElement;
+            if (containerDiv.clientHeight < newHeight) {
+                containerDiv.setAttribute('style', 'height:' + newHeight + 'px');
             }
-
-            event.dataTransfer.clearData();
         }
     }
 }
